fc89b5c4fe8c4a4d39c637e3af326306ac787950
mdsamiulislam54 <samiulm5332@gmail.com>
2025-11-08 21:02:53 +0600

    insertionSort algorithm implement

---
+++ insertionSort.js
@@ -0,0 +1,19 @@
+
+// insertionSort 
+
+const insertionSort =(array)=>{
+    for (let i = 1 ; i < array.length; i++){
+        let current = array[i];
+        let j = i -1;
+        while(j >= 0 && array[j] > current){
+            array[j +1] = array[j]
+            j--;
+        }
+
+        array[ j + 1] = current;
+    }
+
+    return array
+}
+
+console.log(insertionSort([5,4,3,2,1]))
